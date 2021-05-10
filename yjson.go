841a2de158344070d8bd6635b9a9fcc2c38eea7d
颜成有 <875257500@qq.com>
2021-05-10 22:03:29 +0800

    version 1.0

--- yjson.go
+++ yjson.go
@@ -22,7 +22,34 @@ const (
 	True
 	// JSON is a raw block of JSON
 	JSON
+	// Slice is a json SLICE
+	SLICE
+	// map is a json MAP
+	MAP
 )
+
+func (t Type) String() string {
+	switch t {
+	default:
+		return ""
+	case Null:
+		return "Null"
+	case False:
+		return "False"
+	case Number:
+		return "Number"
+	case String:
+		return "String"
+	case True:
+		return "True"
+	case JSON:
+		return "JSON"
+	case SLICE:
+		return "SLICE"
+	case MAP:
+		return "MAP"
+	}
+}
 
 type Result struct {
 	// Type is the json type
@@ -35,48 +62,248 @@ type Result struct {
 	Num float64
 	// Index of raw value in original json, zero means index unknown
 	Index int
+	// len
+	Len int
+	// primitive string
+	primitive string
 }
 
-func Parse(json string) *Result {
+func Parse(jsonStr string) *Result {
 	var result Result
-	json = strings.TrimSpace(json)
-	if strings.Count(json, "{") != strings.Count(json, "}") &&
-		strings.Count(json, "[") != strings.Count(json, "]") {
+	jsonStr = strings.TrimSpace(jsonStr)
+	if strings.Count(jsonStr, "{") != strings.Count(jsonStr, "}") &&
+		strings.Count(jsonStr, "[") != strings.Count(jsonStr, "]") {
 		return &Result{Type: False}
 	}
-	if len(json) == 0 {
+	if len(jsonStr) == 0 {
 		return &Result{Type: Null}
 	}
 
 	result.Type = JSON
-	result.Raw = json
+	r, err := parseToMap(jsonStr)
+	if err != nil {
+		panic(err)
+	}
+	js, err := json.Marshal(r)
+	if err != nil {
+		panic(err)
+	}
+	jsonStr = string(js)
+	result.Raw = jsonStr
+	result.primitive = jsonStr
 	return &result
 }
 
 func (r *Result) Get(searchStr string) *Result {
 	//result, err := ParseToMap(js)
-	subList := strings.Split(searchStr, ".")
-	if len(subList) == 1 {
-		result, err := parseToMap(r.Raw)
-		if err != nil {
-			panic(err)
-		}
-		sub := result[subList[0]]
+	//subList := strings.Split(searchStr, "yanchengyou")
+	//if len(subList) == 1 {
+	result, err := parseToMap(r.Raw)
+	if err != nil {
+		panic(err)
+	}
+	sub, ok := result[searchStr]
+	if !ok {
+		r.Raw = ""
+		r.Type = False
+		return r
+	}
+	switch sub.(type) {
+	case []interface{}:
+		r.Type = SLICE
+		subLen := sub.([]interface{})
+		r.Len = len(subLen)
 		jsonStr, err := json.Marshal(sub)
 		if err != nil {
 			panic(err)
 		}
 		r.Raw = string(jsonStr)
-		switch sub.(type) {
-		case []interface{}:
-			fmt.Println("[]interface{}")
-		case string:
-			r.Type = String
-		}
+	case int, float64:
+		r.Type = Number
+		num := sub.(float64)
+		r.Num = num
+		r.Raw = ""
+	case string:
+		r.Type = String
+		str := sub.(string)
+		r.Str = str
+		r.Raw = ""
+	case map[string]interface{}:
+		r.Type = MAP
+		m := sub.(map[string]interface{})
+		subLen := len(m)
+		jsonStr, _ := json.Marshal(sub)
+		r.Raw = string(jsonStr)
+		r.Len = subLen
 	}
+	//} else {
+	//	//以后写吧
+	//}
 	//a, _ := mapRecMap(result, subList)
 	//return a, nil
 	return r
+}
+
+func (r *Result) GetOfIndex(index int) *Result {
+	if !r.IsArray() {
+		panic(errors.New("err for json"))
+	}
+	array := make([]interface{}, r.Len, r.Len)
+	err := json.Unmarshal([]byte(r.Raw), &array)
+	if err != nil {
+		panic(err)
+	}
+	rStr := array[index]
+	rByte, err := json.Marshal(rStr)
+	if err != nil {
+		panic(err)
+	}
+	r.Raw = string(rByte)
+	r.getInterfateType(rStr)
+	return r
+}
+
+func (r Result) IsArray() bool {
+	if r.Type == SLICE {
+		return true
+	}
+	return false
+}
+
+func recMap(v interface{}, search string, index int) (int, error) {
+	subMap := make(map[string]interface{})
+	switch v.(type) {
+	case map[string]interface{}:
+		subMap = v.(map[string]interface{})
+		for _, vv := range subMap {
+			//fmt.Printf(" ---||---%s ",ii)
+			if vv != "" {
+				a, _ := recMap(vv, search, index)
+				if a > -1 {
+					return a, nil
+				}
+			}
+
+		}
+	case string:
+		mStr := v.(string)
+		if strings.EqualFold(mStr, search) {
+			return index, nil
+		}
+	default:
+		return -2, nil
+	}
+	return -1, nil
+}
+
+// GetIndex 获取index
+func (r *Result) GetIndex(search string) (int, error) {
+	if !r.IsArray() {
+		panic(errors.New("string is not a array！"))
+	}
+	array := make([]interface{}, r.Len, r.Len)
+	err := json.Unmarshal([]byte(r.Raw), &array)
+	if err != nil {
+		panic(err)
+	}
+	for i, v := range array {
+		ii, _ := recMap(v, search, i)
+		if ii != -1 {
+			return i, nil
+		}
+		//switch v.(type) {
+		//case map[string]interface{}:
+		//	subMap = v.(map[string]interface{})
+		//}
+		//for n, m := range subMap {
+		//	if strings.EqualFold(n, search) {
+		//		return i, nil
+		//	}
+		//	switch m.(type) {
+		//	case string:
+		//		mStr := m.(string)
+		//		if strings.EqualFold(mStr, search) {
+		//			return i, nil
+		//		}
+		//	case map[string]interface{}:
+		//
+		//	}
+		//}
+	}
+	return -1, errors.New("没找到！")
+}
+
+func (r *Result) Put(key string, value interface{}, param ...interface{}) {
+	if r.IsArray() {
+		array := make([]interface{}, r.Len, r.Len)
+		err := json.Unmarshal([]byte(r.Raw), &array)
+		if err != nil {
+			panic(err)
+		}
+		index := param[0].(int)
+		str := array[index]
+		arrayByte, _ := json.Marshal(array[index])
+		if mapStr, ok := str.(map[string]interface{}); ok {
+			mapStr[key] = value
+			newStrByte, err := json.Marshal(mapStr)
+			if err != nil {
+				panic(err)
+			}
+			r.Raw = strings.ReplaceAll(r.Raw, string(arrayByte), string(newStrByte))
+			r.primitive = strings.ReplaceAll(r.primitive, string(arrayByte), string(newStrByte))
+			//fmt.Println(r.Raw)
+			//fmt.Println(r.primitive)
+		}
+	} else {
+		mapJson := make(map[string]interface{})
+		err := json.Unmarshal([]byte(r.Raw), &mapJson)
+		if err != nil {
+			panic(err)
+		}
+		mapJson[key] = value
+		newStrByte, err := json.Marshal(mapJson)
+		if err != nil {
+			panic(err)
+		}
+		r.primitive = strings.ReplaceAll(r.primitive, r.Raw, string(newStrByte))
+		r.Raw = strings.ReplaceAll(r.Raw, r.Raw, string(newStrByte))
+	}
+}
+
+func (r Result) Set(value interface{}) {
+	switch value.(type) {
+	case string:
+		strValue := value.(string)
+		r.Str = strValue
+	case int, float64:
+		numValue := value.(float64)
+		r.Num = numValue
+	}
+}
+
+func (r *Result) Array() []interface{} {
+	if !r.IsArray() {
+		return nil
+	}
+	//var resultArray []Result = make([]Result, r.Len, r.Len)
+	array := make([]interface{}, r.Len, r.Len)
+	err := json.Unmarshal([]byte(r.Raw), &array)
+	if err != nil {
+		panic(err)
+	}
+	return array
+	//for _, v := range array {
+	//	switch v.(type) {
+	//	case map[string]interface{}:
+	//		m := sub.(map[string]interface{})
+	//		subLen := len(m)
+	//		jsonStr, _ := json.Marshal(sub)
+	//		r.Raw = string(jsonStr)
+	//		r.Len = subLen
+	//	}
+	//}
+	//resultArray = append(resultArray, resultsub)
+
 }
 
 var result map[string]interface{}
@@ -118,25 +345,20 @@ func mapRecMap(s map[string]interface{}, sl []string) (interface{}, error) {
 	return "err", nil
 }
 
-func PutStr(js string, searchStr string, subKey string, searchValue string, key string,
-	value interface{}) (map[string]interface{}, error) {
-
-	searchResult, _ := Get(js, searchStr)
-	switch searchResult.(type) {
+func (r *Result) getInterfateType(v interface{}) {
+	switch v.(type) {
 	case []interface{}:
-		if m, ok := searchResult.([]interface{}); ok {
-			for _, v := range m {
-				if sub, ok := v.(map[string]interface{}); ok {
-					subValue := sub[subKey].(string)
-					if strings.EqualFold(subValue, searchValue) {
-						sub[key] = value
-					}
-				}
-			}
-		}
-		return result, nil
-	default:
-		fmt.Println(result)
-		return nil, errors.New("断言error")
+		r.Type = SLICE
+	case string:
+		r.Type = String
+	case int, float64:
+		r.Type = Number
+	case map[string]interface{}:
+		r.Type = MAP
 	}
+}
+
+func (r Result) String() string {
+
+	return fmt.Sprintf("%v",r.primitive)
 }
